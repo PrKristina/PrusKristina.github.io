05dcff66a150b4d58e99da99c346a0c40b7ee45b
PrKristina <125187763+PrKristina@users.noreply.github.com>
2023-05-05 20:24:38 +0300

    Update code_9.js

--- week9/code_9.js
+++ week9/code_9.js
@@ -5,7 +5,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 window.addEventListener("DOMContentLoaded", () => {
 
     const scene = new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 3000 );
+    const camera = new THREE.PerspectiveCamera( 35, window.innerWidth / window.innerHeight, 0.1, 3000 );
     const renderer = new THREE.WebGLRenderer({antialias: true});
 
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -28,8 +28,8 @@ window.addEventListener("DOMContentLoaded", () => {
       // called when the resource is loaded
       function ( gltf ) {
          //gltf.scene.scale.set(0.01, 0.01, 0.01);
-         gltf.scene.position.х= 5; 
-         gltf.scene.position.z=-5;
+         gltf.scene.position.х=-15; 
+         gltf.scene.position.z=-10;
          gltf.scene.rotation.y=+Math.PI/4; 
          scene.add( gltf.scene );
           //console.log(gtfl);
