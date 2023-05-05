7da17160d944b29929f8cc7bdec9a1abb83334e3
PrKristina <125187763+PrKristina@users.noreply.github.com>
2023-05-05 20:27:49 +0300

    Update code_9.js

--- week9/code_9.js
+++ week9/code_9.js
@@ -27,9 +27,8 @@ window.addEventListener("DOMContentLoaded", () => {
       'old_car_-_vaz_2107.glb',
       // called when the resource is loaded
       function ( gltf ) {
-         //gltf.scene.scale.set(0.01, 0.01, 0.01);
-         gltf.scene.position.х=-15; 
-         gltf.scene.position.z=-10;
+         //gltf.scene.scale.set(0.01, 0.01, 0.01)
+         gltf.scene.position.z=-1
          gltf.scene.rotation.y=+Math.PI/4; 
          scene.add( gltf.scene );
           //console.log(gtfl);
