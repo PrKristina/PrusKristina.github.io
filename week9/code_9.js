4071e7169ba473d1b4c6e5008c738d3e969f3ae5
PrKristina <125187763+PrKristina@users.noreply.github.com>
2023-05-05 20:21:59 +0300

    Update code_9.js

--- week9/code_9.js
+++ week9/code_9.js
@@ -28,7 +28,7 @@ window.addEventListener("DOMContentLoaded", () => {
       // called when the resource is loaded
       function ( gltf ) {
          //gltf.scene.scale.set(0.01, 0.01, 0.01);
-         gltf.scene.position.х=-5; 
+         gltf.scene.position.х= 5; 
          gltf.scene.position.z=-5;
          gltf.scene.rotation.y=+Math.PI/4; 
          scene.add( gltf.scene );
