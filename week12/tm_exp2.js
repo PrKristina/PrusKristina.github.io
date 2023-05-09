b2d654b5137e565c6b0b5ac00a889183e7644ae6
PrKristina <125187763+PrKristina@users.noreply.github.com>
2023-05-09 23:14:59 +0300

    Update tm_exp2.js

--- week12/tm_exp2.js
+++ week12/tm_exp2.js
@@ -49,30 +49,31 @@
         labelContainer.innerHTML = "Бачу праву руку<br><br>";
 
       // Calculate finger lengths
-      const fingerLengths = [];
-      for (let i = 0; i < hands[0].annotations.indexFinger.length; i++) {
-        const mcp = hands[0].annotations.indexFinger[i][0];
-        const tip = hands[0].annotations.indexFinger[i][3];
-        const length = Math.sqrt(Math.pow(tip[0] - mcp[0], 2) + Math.pow(tip[1] - mcp[1], 2));
-        fingerLengths.push(length);
-      }
-
-      // Calculate angles between fingers
-      const angles = [];
-      for (let i = 0; i < fingerLengths.length - 1; i++) {
-        const v1 = [          hands[0].annotations.indexFinger[i][3][0] - hands[0].annotations.indexFinger[i][0][0],
-          hands[0].annotations.indexFinger[i][3][1] - hands[0].annotations.indexFinger[i][0][1]
-        ];
-        const v2 = [          hands[0].annotations.indexFinger[i + 1][3][0] - hands[0].annotations.indexFinger[i + 1][0][0],
-          hands[0].annotations.indexFinger[i + 1][3][1] - hands[0].annotations.indexFinger[i + 1][0][1]
-        ];
-        const dot = v1[0] * v2[0] + v1[1] * v2[1];
-        const mag1 = Math.sqrt(Math.pow(v1[0], 2) + Math.pow(v1[1], 2));
-        const mag2 = Math.sqrt(Math.pow(v2[0], 2) + Math.pow(v2[1], 2));
-        const angle = Math.acos(dot / (mag1 * mag2));
-        angles.push(angle * 180 / Math.PI);
-      }
+  const fingerLengths = [];
+  if (hands[0].annotations.indexFinger && hands[0].annotations.indexFinger.length > 0) {
+    for (let i = 0; i < hands[0].annotations.indexFinger.length; i++) {
+      const mcp = hands[0].annotations.indexFinger[i][0];
+      const tip = hands[0].annotations.indexFinger[i][3];
+      const length = Math.sqrt(Math.pow(tip[0] - mcp[0], 2) + Math.pow(tip[1] - mcp[1], 2));
+      fingerLengths.push(length);
     }
+
+    // Calculate angles between fingers
+    const angles = [];
+    for (let i = 0; i < fingerLengths.length - 1; i++) {
+      const v1 = [          hands[0].annotations.indexFinger[i][3][0] - hands[0].annotations.indexFinger[i][0][0],
+        hands[0].annotations.indexFinger[i][3][1] - hands[0].annotations.indexFinger[i][0][1]
+      ];
+      const v2 = [          hands[0].annotations.indexFinger[i + 1][3][0] - hands[0].annotations.indexFinger[i + 1][0][0],
+        hands[0].annotations.indexFinger[i + 1][3][1] - hands[0].annotations.indexFinger[i + 1][0][1]
+      ];
+      const dot = v1[0] * v2[0] + v1[1] * v2[1];
+      const mag1 = Math.sqrt(Math.pow(v1[0], 2) + Math.pow(v1[1], 2));
+      const mag2 = Math.sqrt(Math.pow(v2[0], 2) + Math.pow(v2[1], 2));
+      const angle = Math.acos(dot / (mag1 * mag2));
+      angles.push(angle * 180 / Math.PI);
+    }
+  }
     frameCount++;
   }
 }
